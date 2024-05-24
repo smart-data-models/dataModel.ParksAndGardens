45bafa448dc09ba6cfd4429fe697b30965e1e024
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 17:16:52 +0200

    created example of code

--- FlowerBed/code/code_for_using_dataModel.ParksAndGardens_FlowerBed.py
+++ FlowerBed/code/code_for_using_dataModel.ParksAndGardens_FlowerBed.py
@@ -24,21 +24,38 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "FlowerBed"
 subject = "dataModel.ParksAndGardens"
-category = {'type': 'Property', 'value': ['urbanTreeSpot']}
+category = ['urbanTreeSpot']
 attribute = "category"
 value = category
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-dateLastWatering = "{'type': 'Property', 'value': {'@type': 'DateTime', '@value': '2017-03-31T08:00:00Z'}}"
+dateLastWatering = "2017-03-31T08:00:00Z"
 attribute = "dateLastWatering"
 value = dateLastWatering
+# The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
+print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
+
+nextWateringDeadline = "2017-05-31T08:00:00Z"
+attribute = "nextWateringDeadline"
+value = nextWateringDeadline
+# The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
+print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
+
+depth = 0.3
+attribute = "depth"
+value = depth
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
